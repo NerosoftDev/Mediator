2c1f389dedbaf7f97177befc0e201e3d3542d3d9
damon <zhaorong@outlook.com>
2026-05-16 00:09:58 +0800

    Refactor exception handling strategies and improve Validator interface generics

--- src/main/java/com/neroyun/mediator/strategy/HandlerParallelStrategy.java
+++ src/main/java/com/neroyun/mediator/strategy/HandlerParallelStrategy.java
@@ -5,6 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Defines the strategy for executing handlers in parallel within the mediator pattern.
+ * This annotation can be applied to handler classes to specify how they should be executed in parallel when processing messages.
+ * The available strategies include:
+ * 1. NO_WAIT: Runs handlers in parallel without waiting for any of them to complete. This is useful for fire-and-forget scenarios where handlers can execute independently without blocking the main thread or waiting for their results.
+ * 2. WHEN_ALL: Waits for all handlers to complete before proceeding. This is useful when you need to ensure that all handlers have finished processing before moving on to the next step in the workflow. The main thread will block until all handlers have completed their execution.
+ * 3. WHEN_ANY: Waits for any handler to complete before proceeding. This is useful when you need to continue processing as soon as the first handler finishes, without waiting for all handlers to complete. The main thread will block until at least one handler has completed its execution.
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface HandlerParallelStrategy {
@@ -14,21 +22,21 @@ public @interface HandlerParallelStrategy {
      * allowing them to execute independently without blocking the main thread or waiting for their results.
      * Handlers will be executed concurrently, and the main thread will continue immediately after dispatching the handlers.
      */
-    public final String No_WAIT = "NO_WAIT";
+    String No_WAIT = "NO_WAIT";
 
     /**
      * Waits for all handlers to complete before proceeding.
      * This strategy is useful when you need to ensure that all handlers have finished processing before moving on to the next step in the workflow.
      * The main thread will block until all handlers have completed their execution.
      */
-    public final String WHEN_ALL = "WHEN_ALL";
+    String WHEN_ALL = "WHEN_ALL";
 
     /**
      * Waits for any handler to complete before proceeding.
      * This strategy is useful when you need to continue processing as soon as the first handler finishes, without waiting for all handlers to complete.
      * The main thread will block until at least one handler has completed its execution.
      */
-    public final String WHEN_ANY = "WHEN_ANY";
+    String WHEN_ANY = "WHEN_ANY";
 
     String value() default No_WAIT;
 }
